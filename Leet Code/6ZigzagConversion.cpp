1469ddbbf5a0731ecc16d84cf79f841bc61cbc40
Shakil Ahammed <shakilinfo7@gmail.com>
2025-03-04 01:33:38 +0600

    it sucks, it took 1 month to solve while solving other problems

--- Leet Code/6ZigzagConversion.cpp
+++ Leet Code/6ZigzagConversion.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
+
 using namespace std;
 
-int main(){
-    
-}   
+class Solution {
+    public:
+        string convert(string s, int numRows){
+            if(numRows == 1) return s;
+            string ret;
+            int n = s.size();
+            if(n<=numRows) return s;
+            int cycleLen = 2 * numRows - 2;
+
+            for(int i = 0; i < numRows; i++){
+                ret += s[i];
+                int current = i;
+                while (current + cycleLen - 2 * i < n)
+                {
+                    if (i != 0 && i != numRows - 1 && current + cycleLen - 2 * i < n)
+                    {
+                        ret += s[current + cycleLen - 2 * i];
+                    }
+                    current += cycleLen;
+                    if(current < n){
+                        ret += s[current];
+                    }
+                    else break;
+                }
+               
+                
+            }
+            return ret;
+        }    
+
+};
+
+int main() {
+    Solution s;
+    cout << s.convert("PAYPALISHIRING", 3) << endl;
+    return 0;
+}
