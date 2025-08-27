b726820882f362caf4e4322202635aa372de1122
Shakil Ahammed <shakilinfo7@gmail.com>
2025-08-27 22:32:15 +0600

    simple fibonacci

---
+++ Leet Code/509FibonacciNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+int main(){
+    Solution s;
+    int n;
+    cin >> n;
+    cout << s.fib(n) << endl;
+    return 0;
+}
